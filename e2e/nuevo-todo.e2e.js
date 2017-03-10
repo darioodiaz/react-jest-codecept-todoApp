40c96d97655b23139574e18749f43cf0202db009
darioodiaz <darioo.diaz1@gmail.com>
2017-03-10 13:06:54 -0300

    e2e

--- e2e/nuevo-todo.e2e.js
+++ e2e/nuevo-todo.e2e.js
@@ -1,13 +1,9 @@
 
 Feature('Agregar Todo');
 
-Scenario('cargar pagina', (I) => {
+Scenario('cargar pagina y ver 3 todos y un todo vacio', (I) => {
     I.amOnPage('/');
     I.see('Todo App');
-});
-
-Scenario('cargar pagina', (I) => {
-    I.amOnPage('/');
-    I.seeElement('.todo-list');
-    let todos = 
+    I.seeNumberOfElements('.todo-item', 3);
+    I.seeNumberOfElements('txt-new-todo', 1);
 });
