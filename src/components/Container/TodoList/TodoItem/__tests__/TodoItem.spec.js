9cb6d2867bfa519202f696856dadc30317ed0c4b
darioodiaz <darioo.diaz1@gmail.com>
2017-03-10 03:33:15 -0300

    added new tests, and yarn support

--- src/components/Container/TodoList/TodoItem/__tests__/TodoItem.spec.js
+++ src/components/Container/TodoList/TodoItem/__tests__/TodoItem.spec.js
@@ -7,41 +7,51 @@ it('montar el componente sin problemas', () => {
         text: 'Todo',
         selected: false
     };
-    function onSelectItem() {
-    }
-    const div = document.createElement('div');
-    ReactDOM.render(<TodoItem id={0} todo={todo.text} selected={todo.selected} onSelectItem={onSelectItem} />, div);
+    shallow(<TodoItem id={0} todo={todo.text} selected={todo.selected} onSelectItem={jest.fn()} />);
 });
 
 describe('Control de estados', () => {
     let todo = {
-        text: 'Todo',
-        selected: false
+        text: 'Todo predefinido'
     };
-    function onSelectItem() { }
 
     it(`tener texto: ${todo.text}`, () => {
-        const div = document.createElement('div');
-        ReactDOM.render(<TodoItem id={0} todo={todo.text} selected={todo.selected} onSelectItem={onSelectItem} />, div);
-        const todoText = div.querySelector('input[type="text"]').value;
+        const el = shallow(<TodoItem id={0} todo={todo.text} selected={false} onSelectItem={jest.fn()} />);
+        const todoText = el.find('input[name="todo-item-0"]').prop('value');
         expect(todoText).toBe(todo.text);
     });
 
-    it(`no estar seleccionado`, () => {
-        const div = document.createElement('div');
-        ReactDOM.render(<TodoItem id={0} todo={todo.text} selected={todo.selected} onSelectItem={onSelectItem} />, div);
-        const isChecked = div.querySelector('input[type="checkbox"]').checked;
+    it('no estar seleccionado', () => {
+        const el = shallow(<TodoItem id={0} todo={todo.text} selected={false} onSelectItem={jest.fn()} />);
+        const isChecked = el.find('input[type="checkbox"]').prop('checked');
         expect(isChecked).toBeFalsy();
     });
 
-    it(`estar seleccionado`, () => {
-        let todo = {
-            text: 'Todo',
-            selected: true
-        };
-        const div = document.createElement('div');
-        ReactDOM.render(<TodoItem id={0} todo={todo.text} selected={todo.selected} onSelectItem={onSelectItem} />, div);
-        const isChecked = div.querySelector('input[type="checkbox"]').checked;
+    it('estar seleccionado', () => {
+        const el = shallow(<TodoItem id={0} todo={todo.text} selected={true} onSelectItem={jest.fn()} />);
+        const isChecked = el.find('input[type="checkbox"]').prop('checked');
         expect(isChecked).toBeTruthy();
+    });
+
+    it('no estando seleccionado deberia cambia a seleccionado', () => {
+        let todo = { text: 'Todo', selected: false };
+        const onSelectItemSpy = jest.fn();
+        const el = shallow(<TodoItem id={0} todo={todo.text} selected={todo.selected} onSelectItem={onSelectItemSpy} />);
+        el.find('input[type="checkbox"]').simulate('change', { target: { checked: true }, currentTarget: { checked: true } });
+        const state = el.state().selected;
+        expect(state).toBeTruthy();
+    });
+
+    it('no estando seleccionado deberia cambia a seleccionado, luego volver a no estar seleccionado', () => {
+        let todo = { text: 'Todo', selected: false };
+        const onSelectItemSpy = jest.fn();
+        const el = shallow(<TodoItem id={0} todo={todo.text} selected={todo.selected} onSelectItem={onSelectItemSpy} />);
+        el.find('input[type="checkbox"]').simulate('change', { target: { checked: true }, currentTarget: { checked: true } });
+        let state = el.state().selected;
+        expect(state).toBeTruthy();
+
+        el.find('input[type="checkbox"]').simulate('change', { target: { checked: false }, currentTarget: { checked: false } });
+        state = el.state().selected;
+        expect(state).toBeFalsy();
     });
 });
